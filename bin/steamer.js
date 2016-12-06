330b7fec13fd4fb5071c87d0a3e8e387364df2aa
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2016-12-07 01:21:54 +0800

    draft

--- bin/steamer.js
+++ bin/steamer.js
@@ -6,79 +6,86 @@ const fs = require('fs'),
 	  argv = require('yargs').argv,
 	  exec = require('child_process').exec,
 	  execSync = require('child_process').execSync,
-	  _ = require('lodash'),
-	  requireg = require('requireg'),
-	  npm = requireg('npm');
+	  _ = require('lodash');
+	  // requireg = require('requireg'),
+	  // npm = requireg('npm');
 
-const Warning = require('../libs/SteamerErrWarning'),
-	  Logger = require('../libs/SteamerLogger'),
-	  Get = require('../libs/SteamerGet'),
-	  Install = require('../libs/SteamerInstall'),
-	  DevOrPub = require('../libs/SteamerDevOrPub'),
-	  Init = require('../libs/SteamerInit'),
-	  Remove = require('../libs/SteamerRemove');
+// const Warning = require('../libs/SteamerErrWarning'),
+// 	  Logger = require('../libs/SteamerLogger'),
+// 	  Get = require('../libs/SteamerGet'),
+// 	  Install = require('../libs/SteamerInstall'),
+// 	  DevOrPub = require('../libs/SteamerDevOrPub'),
+// 	  Init = require('../libs/SteamerInit'),
+// 	  Remove = require('../libs/SteamerRemove');
+// 	  
 
-var steamerConfigPath = path.resolve('steamer.config.js'),
-	steamerConfig = {},
-	projectConfig = {};
+const Kit = require('../libs/steamer-plugin-kit');
+
+// var steamerConfigPath = path.resolve('steamer.config.js'),
+// 	steamerConfig = {},
+// 	projectConfig = {};
 	// defaultNodeVer = {},
 
 /**
  * read config file
  * @return {[type]} [description]
  */
-function readConfig() {
-	let config = null;
-	if (!fs.existsSync(steamerConfigPath)) {
-		throw new Warning.ConfigMissing();
-	}
+// function readConfig() {
+// 	let config = null;
+// 	if (!fs.existsSync(steamerConfigPath)) {
+// 		throw new Warning.ConfigMissing();
+// 	}
 
-	config = require(steamerConfigPath);
+// 	config = require(steamerConfigPath);
 
-	if (!config) {
-		throw new Warning.ConfigMissing();
-	}
+// 	if (!config) {
+// 		throw new Warning.ConfigMissing();
+// 	}
 
-	return config;
-}
+// 	return config;
+// }
 
 
 function init() {
 
-	if (argv.init) {
-		Init();
+	if (argv.kit) {
+		Kit();
 	}
 
-	steamerConfig = readConfig();
-	projectConfig = steamerConfig.projects;
-	// defaultNodeVer = steamerConfig.defaultNodeVersion;
-
-	if (!projectConfig) {
-		throw new Warning.ProjectMissing();
-	}
-
-	// if (!defaultNodeVer) {
-	// 	throw new Warning.NodeVerMissing();
+	// if (argv.init) {
+	// 	Init();
 	// }
 
-	if (argv.dev || argv.pub) {
-		DevOrPub(steamerConfig);
-	}
-	else if (argv.install) {
-		Install(steamerConfig);
-	}
-	else if (argv.get) {
-		Get(steamerConfig);
-	}
-	else if (argv.remove) {
-		Remove(steamerConfig);
-	}
-	else if (argv.init) {
+	// steamerConfig = readConfig();
+	// projectConfig = steamerConfig.projects;
+	// // defaultNodeVer = steamerConfig.defaultNodeVersion;
 
-	}
-	else {
-		throw new Warning.Command();
-	}
+	// if (!projectConfig) {
+	// 	throw new Warning.ProjectMissing();
+	// }
+
+	// // if (!defaultNodeVer) {
+	// // 	throw new Warning.NodeVerMissing();
+	// // }
+
+	// if (argv.dev || argv.pub) {
+	// 	DevOrPub(steamerConfig);
+	// }
+	// else if (argv.install) {
+	// 	Install(steamerConfig);
+	// }
+	// else if (argv.get) {
+	// 	Get(steamerConfig);
+	// }
+	// else if (argv.remove) {
+	// 	Remove(steamerConfig);
+	// }
+	// else if (argv.init) {
+
+	// }
+	// else {
+	// 	throw new Warning.Command();
+	// }
 }
 
 init();
---
+++ libs/steamer-plugin-kit.js
@@ -0,0 +1,152 @@
+"use strict";
+
+const os = require('os'),
+	  path = require('path'),
+	  fs = require('fs-extra'),
+	  argv = require('yargs').argv,
+	  inquirer = require('inquirer'),
+	  _ = require('lodash');
+
+const isWindows = (os.type() === "Windows_NT");
+
+const prefix = "steamer-",
+	  node_modules = isWindows ? 
+	  				  path.join(process.config.variables.node_prefix + "/node_modules")
+	  				: path.join(process.config.variables.node_prefix + "/lib/node_modules");
+
+function copyFiles(kitPath, folder, tpl) {
+	fs.copySync(path.join(kitPath, "template"), path.join(folder));
+	fs.writeFileSync(path.join(folder, "tools/config.js"), tpl);
+}
+
+function backupFiles(folder, bkFiles) {
+	let destFolder = "backup/" + Date.now();
+	bkFiles.forEach((item, key) => {
+		fs.copySync(path.join(folder, item), path.join(folder, destFolder, item));
+		fs.removeSync(path.join(folder, item));
+	});
+}
+
+function copyFilterFiles(kitPath, folder, tpl, bkFiles) {
+	
+	bkFiles.forEach((item, key) => {
+		fs.copySync(path.join(kitPath, "template", item), path.join(folder, item));
+	});
+
+	fs.writeFileSync(path.join(folder, "tools/config.js"), tpl);
+}
+
+function processTemplate(tpl, config) {
+	let configKeys = Object.keys(config);
+
+	configKeys.map((item, key) => {
+		tpl = tpl.replace("{{" + item + "}}", config[item]);
+	});
+
+	return tpl;
+}
+
+function createLocalConfig(kit, folder, config) {
+	let localConfig = JSON.stringify(config, null, 4);
+	fs.ensureFileSync(path.join(folder, ".steamer/" + kit + ".js"));
+	fs.writeFileSync(path.join(folder, ".steamer/" + kit + ".js"), localConfig, 'utf-8');
+}
+
+function readLocalConfig(folder, kit, configFile) {
+	let isLocalConfigExist = fs.existsSync(configFile);
+
+	try {
+		if (isLocalConfigExist) {
+			let config = fs.readFileSync(configFile, "utf-8");
+			return JSON.parse(config);
+		}
+	}
+	catch(e) {
+		throw("Read local config fail");
+		return false;
+	}
+
+	return false;
+}
+
+function install(opts) {
+
+	let kit = opts.kit,
+		kitPath = opts.kitPath,
+		folder = opts.folder;
+
+	let inquirerConfig = null;
+
+	try {
+		inquirerConfig = require(kit);
+	}
+	catch(e) {
+		throw("The kit " + kit + " is not installed");
+	}
+
+	let configTemplate = fs.readFileSync(path.join(kitPath, "template/tools/config.js"), "utf-8");
+
+	let config = null;
+
+	inquirer.prompt(
+		inquirerConfig
+	).then(function (answers) {
+
+		answers.webserver = "//localhost:9000/";
+		answers.cdn = answers.cdn || answers.webserver;
+		answers.port = answers.port || 9000; 
+		answers.route = answers.route || "/";
+
+		config = _.merge({}, answers);
+
+		configTemplate = processTemplate(configTemplate, config);
+
+		copyFiles(kitPath, folder, configTemplate);
+
+		createLocalConfig(kit, folder, config);
+	});
+}
+
+function update(opts) {
+	let kit = opts.kit,
+		kitPath = opts.kitPath,
+		folder = opts.folder,
+		localConfigFile = opts.localConfigFile,
+		localConfig = opts.localConfig,
+		bkFiles = opts.bkFiles;
+
+	backupFiles(folder, bkFiles);	
+
+	let configTemplate = fs.readFileSync(path.join(kitPath, "template/tools/config.js"), "utf-8");
+	configTemplate = processTemplate(configTemplate, localConfig);
+
+	copyFilterFiles(kitPath, folder, configTemplate, bkFiles);
+}
+
+module.exports = function() {
+	let kit = prefix + argv.kit,
+		kitPath = path.join(node_modules, kit),
+		folder = (argv._.length) ? argv._[0] : kit,
+		localConfigFile = path.join(folder, ".steamer/" + kit + ".js"),
+		bkFiles = ["tools", "README.md", "package.json"];
+
+
+	let localConfig = readLocalConfig(folder, kit, localConfigFile);
+
+	let opts = {
+		kit,
+		kitPath,
+		folder,
+		localConfigFile,
+		localConfig,
+		bkFiles
+	};
+
+	if (localConfig) {
+		update(opts);
+	}
+	else {
+		install(opts);
+	}
+
+};
